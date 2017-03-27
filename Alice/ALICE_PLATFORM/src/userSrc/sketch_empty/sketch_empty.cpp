bd8baebc1ad32fbb2f6767c6dab9414ba8316ff3
shajay.b <shajay.b@ZH-PC-414-06>
2017-03-27 14:28:01 +0100

    March27_gitcreate

--- Alice/ALICE_PLATFORM/src/userSrc/sketch_empty/sketch_empty.cpp
+++ Alice/ALICE_PLATFORM/src/userSrc/sketch_empty/sketch_empty.cpp
@@ -1,46 +1,62 @@
-#include "main.h"
-#include "ALICE_ROBOT_DLL.h"
-using namespace ROBOTICS;
+#define _MAIN_
 
-double zTol = 0.1;
-bool run = false; 
+#ifdef _MAIN_
+
+#include "main.h"
+#include "MODEL.h"
+#include <array>
+#include <memory>
+#include<time.h>
+#include<experimental/generator> 
+
+using namespace std;
+using namespace std::experimental;
+
+
+// model - view - controller (MVC) paradigm / pattern / template 
+
+//////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
 
-	S.addSlider(&zTol, "zTol");
-	B.addButton(&run, "run");
-}
-
-void update(int value)
-{
-	if(run)
-	cout << zTol << endl;
 }
 
 void draw()
 {
 
-	backGround(0.75);
-	drawGrid(20.0);
+	double ht = 350;
+	double ht_plus = 20;
 
+	backGround(0.75);
+	drawGrid(20);
+
+
+}
+
+
+
+void mousePress(int b, int state, int x, int y)
+{
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+void update(int value)
+{
+}
+
+void mouseMotion(int x, int y)
+{
 }
 
 void keyPress(unsigned char k, int xm, int ym)
 {
 
-	
-}
-
-void mousePress(int b, int state, int x, int y)
-{
-
-}
-
-void mouseMotion(int x, int y)
-{
-	
 }
 
 
 
+
+
+#endif // _MAIN_
