9269d3f6e20c4411edec9100c9f8e4a91444edd1
shajay <shajay.bhooshan@gmail.com>
2017-05-17 14:12:15 +0100

    ...

--- Alice/ALICE_PLATFORM/src/userSrc/sketch_helloWorld/sketch_empty.cpp
+++ Alice/ALICE_PLATFORM/src/userSrc/sketch_helloWorld/sketch_empty.cpp
@@ -17,47 +17,66 @@ using namespace std::experimental;
 ///////// ----------------------------------------- model - view - controller (MVC) paradigm / pattern / template  ----------------- ////////////////////////////// 
 /////////////////////////// model  ///////////////////////////////////////////
 
-class circle
+class dot
 {
 public:
-	float radius;
+	//class attributes / properties 
+	vec position; 
+	vec vel;
+	vec trail[100];
+	int cnt = 0;
 
+	void move()
+	{
+		vel.x += ofRandom(-0.1, 0.1);
+		vel.y += ofRandom(-0.1, 0.1);
+		vel.z += ofRandom(-0.1, 0.1);
+		
+		trail[cnt++] = position;
+		if (cnt >= 100)cnt = 0;
+
+		position += vel * 0.1;
+	}
+	//class methods
 	void display()
 	{
-		for (int i = 0; i < 100; i++)
+		glPointSize(5);
+			drawPoint(position);
+			drawLine(position, position + vel.normalise());
+		glPointSize(1);
+
+		for (int i = 1; i < 100; i++)
 		{
-			vec A = vec(radius * sin(2 * PI / 100 * i), radius * cos(2 * PI / 100 * i), 0);
-			vec nextA = vec(radius * sin(2 * PI / 100 * (i+1)), radius * cos(2 * PI / 100 * (i+1)), 0);
-
-			drawPoint(A);
-
-			float r, g, b;
-			r = ofMap(A.x, -radius, radius, 0, 1);
-			g = ofMap(A.y, -radius, radius, 0, 1);
-			b = ofMap(A.z, -radius, radius, 0, 1);
-			glColor3f(r, g, b);
-			drawLine(A, nextA);
+			float r = ofMap(i, 0, 100, 0, 1);
+			glColor3f(r, 0, 0);
+			drawPoint(trail[i]);
 		}
 	}
 };
 
 
-
 // class attributes : radius ;
 // class methods or class actions : draw()
+// instances 
 
+ // OBJECT-ORIENTED PROGAMMING 
+//
 //////////////////////////////////////////////////////////////////////////
-circle myC;
-circle allCircles[100];
+dot allDots[10000]; // 1000 empty dots, with inital position at 0,0,0
 
 void setup()
 {
+	for (int i = 0; i < 10000; i++)allDots[i].position = vec(ofRandom(-10, 10), ofRandom(-10, 10), 0);
 
 }
 
 void update(int value)
 {
 
+	for (int i = 0; i < 100; i++)
+	{
+		allDots[i].move();
+	}
 
 }
 
@@ -70,15 +89,10 @@ void draw()
 	backGround(0.75);
 	drawGrid(20);
 
-	myC.radius = 10.1;
-	myC.display();
 
-
-
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 1000; i++)
 	{
-		allCircles[i].radius = i; 
-		allCircles[i].display();
+		allDots[i].display();
 	}
 	
 }
@@ -86,6 +100,7 @@ void draw()
 /////////////////////////// control  ///////////////////////////////////////////
 void mousePress(int b, int state, int x, int y)
 {
+
 }
 
 void mouseMotion(int x, int y)
@@ -95,7 +110,6 @@ void mouseMotion(int x, int y)
 void keyPress(unsigned char k, int xm, int ym)
 {
 
-	
 
 
 }
