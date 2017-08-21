5c6776a8e08034c8087caac7f834774773807a40
shajay <shajay.bhooshan@gmail.com>
2017-08-21 21:48:29 +0200

    ...

--- Alice/ALICE_PLATFORM/src/userSrc/sketch_helloWorld/sketch_helloWorld.cpp
+++ Alice/ALICE_PLATFORM/src/userSrc/sketch_helloWorld/sketch_helloWorld.cpp
@@ -1,4 +1,6 @@
-#define  _MAIN_
+#define _MAIN_
+
+
 
 #ifdef _MAIN_
 
@@ -16,17 +18,120 @@ using namespace std::experimental;
 
 ///////// ----------------------------------------- model - view - controller (MVC) paradigm / pattern / template  ----------------- ////////////////////////////// 
 /////////////////////////// model  ///////////////////////////////////////////
-vec A(5, 0, 0);
-list<vec> A_pts;
-float inc = 0.005;
+// idea : make 100 points, make them move towards nearest neighbor ;
+
+
+
+class circle
+{
+public:
+	vec cen;
+	vec pts[100];
+	bool detectedOrNot[100];
+
+	///////// setup methods 
+	void initialisePoints() // out points on a circle 
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			pts[i] = vec(
+				1.0 * sin(2 * PI * 1 / 100 * i), // x coordinate
+				1.0 * cos(2 * PI * 1 / 100 * i), // y coordinate
+				0 // z coordinate
+			);
+
+			pts[i] = pts[i] + cen; // move points relative to the location of the center
+		}
+	}
+
+	///////// update methods 
+
+	void reset()
+	{
+		for (int i = 0; i < 100; i++)detectedOrNot[i] = false;
+	}
+	void detect(circle &other)
+	{
+		for (int i = 0; i < 100; i++)
+		{
+
+			for (int j = 0; j < 100; j++)
+			{
+				if (pts[i].distanceTo(other.pts[j]) < .5)
+				{
+					detectedOrNot[i] = true;
+					break;
+				}
+			}
+		}
+	}
+	void expand()
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			if (!detectedOrNot[i])
+				pts[i] += (pts[i] - cen).normalise() * 0.1;
+		}
+	}
+
+	///////// display methods 
+	void display() // display the center and display the points 
+	{
+		drawPoint(cen);
+		for (int i = 0; i < 100; i++)
+		{
+			drawPoint(pts[i]);
+		}
+	}
+
+	///////// export methods
+};
+
+
+
+///////////////////////////////////////////////////////////// MAIN PROGRAM /////////////////////////////////////////////////////////////
+
+
+circle allCircles[4];
+int numOfCircles = 4;
+
+//API application programming interface
 
 void setup()
 {
 
+	for (int i = 0; i < numOfCircles; i++)
+	{
+		allCircles[i].cen = vec(ofRandom(-10, 10), ofRandom(-10, 10), 0);// vec(5 * i, 0, 0);
+		allCircles[i].initialisePoints();
+	}
+
+
 }
+
 
 void update(int value)
 {
+
+	for (int i = 0; i < numOfCircles; i++)
+	{
+		allCircles[i].reset();
+	}
+
+	for (int i = 0; i < numOfCircles; i++)
+	{
+		for (int j = 0; j < numOfCircles; j++)
+		{
+			if (i == j)continue;
+
+			allCircles[i].detect(allCircles[j]);
+		}
+	}
+
+	for (int i = 0; i < numOfCircles; i++)
+	{
+		allCircles[i].expand();
+	}
 
 
 }
@@ -40,40 +145,11 @@ void draw()
 	backGround(0.75);
 	drawGrid(20);
 
+	glPointSize(5);
+	for (int i = 0; i < numOfCircles; i++)allCircles[i].display();
 	glPointSize(1);
 
-	// update rule for a circle
-	vec tangent;
-	tangent.x = (A.y) * 0.01;
-	tangent.y = (A.x) * -0.01;
-	/*tangent.x = ofRandom(-0.1, 0.1);
-	tangent.y = ofRandom(-0.1, 0.1);*/
 
-	vec Anew = A + tangent;
-	A = Anew;
-
-	int numberofPts = A_pts.size();
-	int remainder = numberofPts % 10;
-
-	if (remainder == 0) // every 1000 points
-	{
-		inc = inc * -1;
-		//A = A * (1.0 + inc); // 1.0005 --> 1.0 + 0.0005
-	}
-
-	A = A * (1.0 + inc);
-
-	drawPoint(A);
-
-	A_pts.push_back(A);
-	// principle of differential curves
-	// --> find the tangent of the curve  
-
-	// draw all the points in A_pts ;
-	for (auto pt : A_pts)
-	{
-		drawPoint(pt);
-	}
 }
 
 /////////////////////////// control  ///////////////////////////////////////////
@@ -83,14 +159,13 @@ void mousePress(int b, int state, int x, int y)
 
 void mouseMotion(int x, int y)
 {
+
 }
 
 void keyPress(unsigned char k, int xm, int ym)
 {
 
 
-
-	// update rule for some other curve
 
 
 }
